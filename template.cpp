62633162c0dc014fd8b7244ae4f209179b553ac5
testzer0 <45514239+testzer0@users.noreply.github.com>
2020-08-13 19:05:43 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define ll 				long long
+#define pb 				push_back
+#define mp 				make_pair
+#define pii 			pair<int,int>
+#define vi 				vector<int>
+#define all(a) 			(a).begin(), (a).end()
+#define F 				first
+#define S 				second
+#define sz(x) 			(int)x.size()
+#define rep(i,a,b)  	for(int i = a; i < b; i++)
+#define repn(i,n) 		for(int i = 0; i < n; i++)
+#define doall(e,S) 		for(auto e : (S))
+#define repvec(i,v) 	for(int i = 0; i < v.size(); i++)
+#define print(s)		for(auto e : s) cout << e << ' ' ; cout << endl;
+#define isset(n, b) 	((n >> b) & 1)
+using namespace std;
+
+const long long int hell  = 		1000000007;
+
+void solve() {
+
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifdef LOCAL
+        freopen("in.txt", "r", stdin);
+        freopen("out.txt", "w", stdout);
+    #endif
+	int T;
+	cin >> T;
+	while (T--)
+		solve();
+	return 0;
+}
